5f668dd28e7e609e0c460aebd1a2141c604232e5
Chris Martinez <chris@chrismartinez.net>
2023-01-31 14:47:22 -0600

    final again

--- routes/regexmatch/middleware.js
+++ routes/regexmatch/middleware.js
@@ -1,42 +1,46 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { query, validationResult } from "express-validator";
 const router = Router();
 
-router.get('/routinematch', (req, res) => {
-  const validProperties = ["class1", "class2", "class3", "routineName"]
-  const validRequest = validProperties.every(prop => req.query.hasOwnProperty(prop));
-  if (validRequest){
-    const routineName = req.query.routineName.toUpperCase().replace(/\s+/g, '')
+router.get(
+  "/routinematch",
+  query("class1").isBoolean(),
+  query("class2").isBoolean(),
+  query("class3").isBoolean(),
+  query("routineName").trim().escape().isLength({ min: 2, max: 12 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const routineName = req.query.routineName.toUpperCase().replace(/\s+/g, "");
     const obj = {
-      class1: (req.query.class1 === "true"),
-      class2: (req.query.class2 === "true"),
-      class3: (req.query.class3 === "true")
+      class1: req.query.class1 === "true",
+      class2: req.query.class2 === "true",
+      class3: req.query.class3 === "true",
+    };
+
+    if (obj[determineClass(routineName)]) {
+      res.status(200).json({ response: true });
+    } else {
+      res.status(200).json({ response: false });
     }
 
-    if (obj[determineClass(routineName)]){
-      res.status(200).json({response: true});
-    }
-    else{
-      res.status(200).json({response: false});
+    function determineClass(str) {
+      const string = str.toUpperCase().replace(/\s+/g, "");
+      const patternClass3 = /^A[A-Z]/;
+      const patternClass2 = /^R[1-4]/;
+
+      switch (true) {
+        case patternClass3.test(string):
+          return "class3";
+        case patternClass2.test(string):
+          return "class2";
+        default:
+          return "class1";
+      }
     }
   }
-  else{
-    res.status(500).json({message: "Invalid API request."});
-  }
-
-  function determineClass(str) {
-    const string = str.toUpperCase().replace(/\s+/g, '');
-    const patternClass3 = /^A[A-Z]/;
-    const patternClass2 = /^R[1-4]/;
-
-    switch (true) {
-      case (patternClass3.test(string)):
-        return "class3";
-      case (patternClass2.test(string)):
-        return "class2";
-      default:
-        return "class1";
-    }
-  };
-});
+);
 
 export default router;
