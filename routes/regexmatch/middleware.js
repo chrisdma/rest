c07eae2501eda98fa6f4eb00a11d4ef067d70b00
Chris Martinez <chris@chrismartinez.net>
2023-02-06 15:39:10 -0600

    update with POST

--- routes/regexmatch/middleware.js
+++ routes/regexmatch/middleware.js
@@ -13,7 +13,7 @@ router.get(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const routineName = req.query.routineName.toUpperCase().replace(/\s+/g, "");
+    const routineName = req.query.routineName.toUpperCase();
     const obj = {
       class1: req.query.class1 === "true",
       class2: req.query.class2 === "true",
@@ -27,7 +27,47 @@ router.get(
     }
 
     function determineClass(str) {
-      const string = str.toUpperCase().replace(/\s+/g, "");
+      const string = str.toUpperCase();
+      const patternClass3 = /^A[A-Z]/;
+      const patternClass2 = /^R[1-4]/;
+
+      switch (true) {
+        case patternClass3.test(string):
+          return "class3";
+        case patternClass2.test(string):
+          return "class2";
+        default:
+          return "class1";
+      }
+    }
+  }
+);
+router.post(
+  "/routinematch",
+  body("class1").isBoolean(),
+  body("class2").isBoolean(),
+  body("class3").isBoolean(),
+  body("routineName").trim().escape().isLength({ min: 2, max: 12 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const routineName = req.body.routineName.toUpperCase();
+    const obj = {
+      class1: req.body.class1 === "true",
+      class2: req.body.class2 === "true",
+      class3: req.body.class3 === "true",
+    };
+
+    if (obj[determineClass(routineName)]) {
+      res.status(200).json({ response: true });
+    } else {
+      res.status(200).json({ response: false });
+    }
+
+    function determineClass(str) {
+      const string = str.toUpperCase();
       const patternClass3 = /^A[A-Z]/;
       const patternClass2 = /^R[1-4]/;
 
